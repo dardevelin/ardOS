e07732fb024b213c457425c51d528cc96c8a184d
Colin Tan <colin.ky.tan@gmail.com>
2013-05-16 08:33:22 +0800

    New distribution update

--- source/mutex.c
+++ source/mutex.c
@@ -2,7 +2,25 @@
  * mutex.c
  * Provides mutex and conditional variable support
  * Created: 8/5/2013 8:47:44 AM
- *  Author: dcstanc
+ *  Author: Colin Tan
+
+   	Copyright (C) 2013 Colin Tan
+   	
+   	
+   	This file is part of ArdOS.
+
+   	ArdOS is free software: you can redistribute it and/or modify
+   	it under the terms of the GNU Lesser General Public License as published by
+   	the Free Software Foundation, either version 3 of the License, or
+   	(at your option) any later version.
+
+   	ArdOS is distributed in the hope that it will be useful,
+   	but WITHOUT ANY WARRANTY; without even the implied warranty of
+   	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   	GNU Lesser General Public License for more details.
+
+   	You should have received a copy of the GNU Lesser General Public License
+   	along with ArdOS.  If not, see <http://www.gnu.org/licenses/>.
  */ 
 
 #include "ArdOS.h"
